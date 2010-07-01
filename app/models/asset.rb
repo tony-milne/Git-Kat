12b9d91a16c5eeabfcb60637d30cc53bff0e576e
Albert <albert.osei.09@aberdeen.ac.uk>
2010-07-01 14:41:12 +0100

    commit evereything

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,8 @@
 class Asset < ActiveRecord::Base
   has_attached_file :file,
-      :styles => {:thumb => "230x173#"}
+      :styles => {:thumb => "230x173#",
+      :large => "640x480#>",
+      :medium => "150x150#"}
 
   def self.search(search)
 if search
