273e25b0fc5ef1c705b282a3c9ec0d158676c181
Ryan Glass <ryan.glass.09@aberdeen.ac.uk>
2010-07-08 18:08:23 +0100

    merge fix

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100708131350) do
+ActiveRecord::Schema.define(:version => 20100707014505) do
 
   create_table "assets", :force => true do |t|
     t.integer  "exif_id"
@@ -36,11 +36,6 @@ ActiveRecord::Schema.define(:version => 20100708131350) do
   end
 
   create_table "notifiers", :force => true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tasks", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
