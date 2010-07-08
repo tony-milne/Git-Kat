210cfceea3e881f4d51cd75311799e790b6450a5
Tony Milne <quintus.thorn@googlemail.com>
2010-07-08 03:28:02 +0100

    cleaned up asset model; refactored exif? method; added check in assets/show for exif data

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,4 @@
 class Asset < ActiveRecord::Base
-  #has_one :image
-  #has_one :video
   belongs_to :exif, :polymorphic => true, :dependent => :destroy
   
   has_attached_file :file,
@@ -8,41 +6,50 @@ class Asset < ActiveRecord::Base
   #For windows systems, the greater than sign '>' must be escaped with the hat '^' symbol
   #The '^' is not required on *nix systems
   :styles => {:thumb => "230x173#", :medium => "350x350#", :large => "600x480^>"}
-      
+  
   cattr_reader :per_page
   @@per_page = 10
 
 
-def self.search(search, page)
-  paginate 	:per_page => 3, :page => page,
-		:conditions => ['title like ?', "%#{search}%"],
-		:order => 'title'
-end
-
-def set_exif_data
-  if file_content_type =~ /^image.*/
-    if file_content_type =~ /jpeg/
-      set_image_exif_data
+  def self.search(search, page)
+    paginate 	:per_page => 3, :page => page,
+      :conditions => ['title like ?', "%#{search}%"],
+      :order => 'title'
+  end
+  
+  
+  ## Start of EXIF code ##
+  
+  # Returns true if asset has EXIF data associated
+  def exif?
+    return true if not exif_type.nil?
+    rescue
+      false
+  end
+  
+  def set_exif_data
+    if file_content_type =~ /^image.*/
+      if file_content_type =~ /jpeg/
+        set_image_exif_data
+      end
     end
   end
-end
 
-private
+
+  private
   def set_image_exif_data
-    exif = EXIFR::JPEG.new(self.file.path)
-    return if exif.nil? or not exif.exif?
-      #i = self.create_image
+    exif_data = EXIFR::JPEG.new(file.path)
+    return if exif_data.nil? or not exif_data.exif?
       i = Image.new
-      i.width            = exif.width
-      i.height           = exif.height
-      i.camera_brand     = exif.make
-      i.camera_model     = exif.model
-      i.exposure_time    = exif.exposure_time.to_s
-      i.f_number         = exif.f_number.to_f
-      i.iso_speed_rating = exif.iso_speed_ratings
-      i.shot_date_time   = exif.date_time
-      i.focal_length     = exif.focal_length.to_f
-      #i.save
+      i.width            = exif_data.width
+      i.height           = exif_data.height
+      i.camera_brand     = exif_data.make
+      i.camera_model     = exif_data.model
+      i.exposure_time    = exif_data.exposure_time.to_s
+      i.f_number         = exif_data.f_number.to_f
+      i.iso_speed_rating = exif_data.iso_speed_ratings
+      i.shot_date_time   = exif_data.date_time
+      i.focal_length     = exif_data.focal_length.to_f
       self.exif = i
       self.save!
     rescue
