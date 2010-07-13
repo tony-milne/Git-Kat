ebc1fb6cf2d2b04e9ceb06d06ab5ef4acc3151f0
Graham Bruno <g.bruno.09@aberdeen.ac.uk>
2010-07-13 15:35:58 +0100

    Further changes to namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ ActionController::Routing::Routes.draw do |map|
   
   map.namespace :asset_manager do |manager|    
   manager.resources :assets, :collection => {:gallery => :get}
-  
-  manager.login "login", :controller => "user_sessions", :action => "new"
-  manager.logout "logout", :controller => "user_sessions", :action => "destroy"
-  
   manager.resource :password_reset
-  manager.resources :user_sessions
+  #manager.resources :user_sessions
   manager.resources :users
   end
   
+
+  map.resources :user_sessions
+  map.login "login", :controller => "user_sessions", :action => "new"
+  map.logout "logout", :controller => "user_sessions", :action => "destroy"
   map.root :controller => "AssetManager::Assets"
 
 end
