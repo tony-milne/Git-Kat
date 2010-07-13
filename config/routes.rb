0d3bfc49ffc159d8492533edcad273e664a59a5f
Graham Bruno <g.bruno.09@aberdeen.ac.uk>
2010-07-13 10:41:56 +0100

    started changing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :assets, :collection => {:gallery => :get}
   
-  map.login "login", :controller => "user_sessions", :action => "new"
-  map.logout "logout", :controller => "user_sessions", :action => "destroy"
   
-  map.resource :password_reset
-  map.resources :user_sessions
-  map.resources :users
-  map.root :assets
+  map.namespace :asset_manager do |manager|    
+  manager.resources :assets, :collection => {:gallery => :get}
+  
+  manager.login "login", :controller => "user_sessions", :action => "new"
+  manager.logout "logout", :controller => "user_sessions", :action => "destroy"
+  
+  manager.resource :password_reset
+  manager.resources :user_sessions
+  manager.resources :users
+  end
+  
+  map.root :controller => "AssetManager::Assets"
+
 end
