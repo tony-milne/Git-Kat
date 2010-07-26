a2928c509dec9b06286905e9f489b18a7faafcda
Tony Milne <quintus.thorn@googlemail.com>
2010-07-27 00:03:21 +0100

    started adding in validations

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -1,4 +1,6 @@
 class Caption < ActiveRecord::Base
   belongs_to :language
   belongs_to :asset
+  
+  validates_presence_of :language_id, :message => "A language for the caption must be selected"
 end
