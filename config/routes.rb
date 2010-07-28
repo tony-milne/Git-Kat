40c4c5482ad05c78179f8e5f5da1d53107c31c75
Obelema <obelema.akobo.09@aberdeen.ac.uk>
2010-07-28 14:36:19 +0100

    created new country model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
   ActionController::Routing::Routes.draw do |map|
-<<<<<<< HEAD
+  map.resources :countries
+
   map.resources :tribes
 
   #map.resources :tags
   #map.resources :credits
-  #map.resources :captions
+  #map.resources :captions 
   
-=======
-    
->>>>>>> d1b8e47f1f22cdbeb74b5974172e154fa4b75ae8
   map.namespace :asset_manager do |manager|    
   manager.resources :assets, :collection => {:gallery => :get}, :has_many => [:captions, :credits, :tags]
   manager.resource :password_reset
@@ -18,6 +16,7 @@
   manager.resources :languages, :has_many => :captions
   manager.resources :users
   manager.resources :tags
+  manager.resources :tribes, :has_many => :assets
   end
 
   map.resources :user_sessions
