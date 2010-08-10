4f1ed6f6342cfa735aec6f8759302d53b729aec2
Simon Doolin <simon.p.doolin.08@aberdeen.ac.uk>
2010-08-10 12:28:36 +0100

    Some testing done with MySQL error for Tony to look at

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -49,8 +49,42 @@ class ActiveSupport::TestCase
     UserSession.find.destroy
   end
 
+
+ 
+
+
+
 end
 
 class ActionController::TestCase
   setup :activate_authlogic
 end
+
+def setup_test
+  @verification_errors = []
+  @selenium = Selenium::Client::Driver.new \
+    :host => "localhost",
+    :port => 4444,
+    :browser => "*chrome",
+    :url => "http://localhost:3001",
+    :timeout_in_second => 60
+
+  @selenium.start_new_browser_session
+  end
+
+  def registration_test
+  @selenium.open "/login"
+  @selenium.click "link=register"
+  @selenium.wait_for_page_to_load "30000"
+  @selenium.type "user_username", "test"
+  @selenium.type "user_email", "test@test.com"
+  @selenium.type "user_password", "test1"
+  @selenium.type "user_password_confirmation", "test1"
+  @selenium.click "user_submit"
+  @selenium.wait_for_page_to_load "30000"
+  end
+
+    def teardown_test
+  @selenium.close_current_browser_session
+  assert_equal [], @verification_errors
+  end
--- test/unit/registration_test.rb
+++ test/unit/registration_test.rb
@@ -11,7 +11,7 @@ class RegistrationTest < Test::Unit::TestCase
       :host => "localhost",
       :port => 4444,
       :browser => "*chrome",
-      :url => "http://localhost:3001/",
+      :url => "http://localhost:3001",
       :timeout_in_second => 60
 
     @selenium.start_new_browser_session
@@ -22,14 +22,14 @@ class RegistrationTest < Test::Unit::TestCase
     assert_equal [], @verification_errors
   end
   
-  def test_user_registration
+  def registration_test
     @selenium.open "/login"
     @selenium.click "link=register"
     @selenium.wait_for_page_to_load "30000"
-    @selenium.type "user_username", "Test"
+    @selenium.type "user_username", "test"
     @selenium.type "user_email", "test@test.com"
-    @selenium.type "user_password", "test123"
-    @selenium.type "user_password_confirmation", "test123"
+    @selenium.type "user_password", "test1"
+    @selenium.type "user_password_confirmation", "test1"
     @selenium.click "user_submit"
     @selenium.wait_for_page_to_load "30000"
   end
