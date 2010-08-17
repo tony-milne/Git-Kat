5e5fc9a360e0b8f5d40d0eb96a125c0d6010be8f
Tony Milne <t.milne.09@aberdeen.ac.uk>
2010-08-17 12:44:04 +0100

    dec auth WIP

---
+++ db/migrate/20100817113852_create_admin_users.rb
@@ -0,0 +1,18 @@
+class CreateAdminUsers < ActiveRecord::Migration
+  def self.up
+    create_table :admin_users do |t|
+      t.string :username
+      t.string :email
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.integer :login_count
+      t.integer :failed_login_count
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :admin_users
+  end
+end
