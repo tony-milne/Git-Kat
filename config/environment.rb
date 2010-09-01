97f4dfc51233e69fd761aef2d7f83676a3b7f614
Simon Doolin <simon.p.doolin.08@aberdeen.ac.uk>
2010-09-01 16:02:16 +0100

    Test Fixed and Running

--- config/environment.rb
+++ config/environment.rb
@@ -30,8 +30,7 @@ Rails::Initializer.run do |config|
   config.gem "declarative_authorization"
   config.gem "paperclip"
   config.gem "exifr"
-  config.gem "selenium-client"
-  config.gem "selenium-webdriver"
+  
   config.gem "mysql"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
